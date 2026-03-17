2ece1500e7989edd5dda486c494c77cd6471b57e
aliengaard <202304665@post.au.dk>
2026-03-17 16:53:27 +1000

    Initial datastructure defined interface and data array

---
+++ Part1/app.ts
@@ -0,0 +1,20 @@
+/* Author: Astrid Liengaard Nielsen 
+   Student ID: 25037591
+   Assessment 2: Part 1 */ 
+
+/* Interface for Inventory Items */
+interface InventoryItem {
+    itemId: string;
+    itemName: string;
+    category: 'Electronics' | 'Furniture' | 'Clothing' | 'Tools' | 'Miscellaneous';
+    quantity: number;
+    price: number;
+    supplierName: string;
+    stockStatus: 'In Stock' | 'Low Stock' | 'Out of Stock';
+    popularItem: 'Yes' | 'No';
+    comments: string;
+}
+
+/* Array to hold inventory items */
+let inventory: InventoryItem[] = [];
+
