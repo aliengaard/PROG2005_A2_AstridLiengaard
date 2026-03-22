6373d1546425fe74084b481396997387e48552d2
aliengaard <202304665@post.au.dk>
2026-03-23 09:52:54 +1000

    Added logic to HTML structure in index.html with the input fields, search section and display of the itemlist

--- Part1/app.ts
+++ Part1/app.ts
@@ -18,3 +18,14 @@ interface InventoryItem {
 /* Array to hold inventory items */
 let inventory: InventoryItem[] = [];
 
+/* Delete and Add buttons */ 
+
+`<tr>
+    <td>${InventoyItem.itemName}</td>
+    <td>${item.price}</td>
+    <td>
+        <button onclick="handleUpdate('${item.itemId}')">Update</button>
+        <button class="delete-btn" onclick="handleDelete('${item.itemId}')">Delete</button>
+    </td>
+</tr>`
+
